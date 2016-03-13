69923ea720f1fd0fbf24b6a80fe41c136a34d944
Roy Rapoport <rrapoport@netflix.com>
2016-03-12 22:42:42 -0800

    further stats

--- last_week/comparator.py
+++ last_week/comparator.py
@@ -23,26 +23,44 @@ lp = last.payload
 pp = previous.payload
 
 
-def comparator(pretty, key, lp, pp):
+def comparator(pretty, key, lp, pp, show_percent=True, is_percent=False):
     m = "We had {} {} vs {} the week before, a change of {:.1f}%"
-    ls = lp[key]
-    ps = pp[key]
+    ls = float(lp[key])
+    ps = float(pp[key])
     per = ls * 100.0 / ps - 100
     if per < 0:
         per = -1 * per
         verb = "dropped"
     else:
         verb = "rose "
-    m = "{} {} {:.1f}% from {} to {}".format(pretty, verb, per, ps, ls)
+    if is_percent:
+        ps = "{}%".format(ps)
+        ls = "{}%".format(ls)
+    m = "{} {} from {} to {}".format(pretty, verb, ps, ls)
+    if show_percent:
+        m = "{} {} *{:.1f}%* from {} to {}".format(pretty, verb, per, ps, ls)
     print m
 
+print ""
+print "I just posted last week's stats in #zmeta-statistics.  A brief summary:"
 print ""
 m = "@{} was in first place with {} messages, followed by @{} with {} messages"
 first = lp['users'][0]
 second = lp['users'][1]
 print m.format(first['name'], first['messages'], second['name'], second['messages'])
+
+m = "\nThis is in comparison to the week prior, where "
+m += "@{} was in first place with {} messages, followed by @{} with {} messages"
+first = pp['users'][0]
+second = pp['users'][1]
+print m.format(first['name'], first['messages'], second['name'], second['messages'])
+print ""
+
 comparator("active users", "active_users", lp, pp)
 comparator("total messages", "total_messages", lp, pp)
 comparator("messages from undetermined-gender posters", "undetermined_gender_message_count", lp, pp)
-comparator("messages from female-gender posters", "female_gender_message_count", lp, pp)
-comparator("messages from male-gender posters", "male_gender_message_count", lp, pp)
+comparator("messages from undetermined-gender posters as percentage of total", "undetermined_gender_message_percentage", lp, pp, show_percent=False, is_percent=True)
+comparator("messages from female posters", "female_gender_message_count", lp, pp)
+comparator("messages from female posters as percentage of total", "female_gender_message_percentage", lp, pp, show_percent=False, is_percent=True)
+comparator("messages from male posters", "male_gender_message_count", lp, pp)
+comparator("messages from male posters as percentage of total", "male_gender_message_percentage", lp, pp, show_percent=False, is_percent=True)
