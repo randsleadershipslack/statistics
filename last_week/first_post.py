afa87bd6ae5727606c03988a20ce2a24b7df2e0b
Roy Rapoport <rrapoport@netflix.com>
2016-03-13 12:55:13 -0700

    code to figure out when a user's first post date was, and data so far

---
+++ last_week/first_post.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python2.7
+
+import json
+import os
+import os.path
+import time
+
+import last_week
+
+lw = last_week.LastWeek()
+user_ids = lw.users
+
+cache_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache")
+
+files = os.listdir(cache_dir)
+print "Found {} files".format(len(files))
+
+users = {}
+
+for fname in files:
+    fqfn = os.path.join(cache_dir, fname)
+    fobj = open(fqfn, "r")
+    payload = fobj.read()
+    fobj.close()
+    messages = json.loads(payload)
+    for message in messages:
+        if 'user' not in message:
+            continue
+        user_id = message['user']
+        user_name = user_ids[user_id]
+        ts = int(message['ts'].split('.', 2)[0])
+        pretty_date = time.asctime(time.localtime(ts))
+        payload = {'name': user_name, 'ts': ts, 'datetime': pretty_date}
+        if user_id not in users:
+            users[user_id] = payload
+        else:
+            if ts < users[user_id]['ts']:
+                users[user_id] = payload
+
+    print "Processed {}".format(fqfn)
+
+f = open("user_first_post.json", "w")
+f.write(json.dumps(users, indent=4))
+f.close()
